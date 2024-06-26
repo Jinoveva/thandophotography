9433a3ea3249cf4ea611af5f1c266e8eab4095eb
Jinoveva <jinovevalopes@gmail.com>
2024-06-26 18:30:57 +0200

    added some js function for the contact button

---
+++ src/about.js
@@ -0,0 +1,8 @@
+  // Get the button element by its ID
+        var button = document.getElementById("contact-button");
+
+        // Add an event listener to the button
+        button.addEventListener("click", function() {
+            // Redirect to the desired page
+            window.location.href = "contact.html";
+        });
