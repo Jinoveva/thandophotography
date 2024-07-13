3f4b0346cb09bf978b40f253b82a267c725c6263
Jinoveva <jinovevalopes@gmail.com>
2024-07-13 08:02:41 +0200

    removed email api

--- src/contact.js
+++ src/contact.js
@@ -2,16 +2,3 @@
             window.location.href = 'index.html'; // Replace 'home.html' with your home page URL
         }
 
-        function sendEmail() {
-                Email.send({
-                    Host: "smtp.gmail.com",
-                    Username: "thandohphotography@gmail.com",
-                    Password: "5359C0BC9CA2571B5D04BE83FB64A4239C63",
-                    To: 'Thandohphotography@gmail.com',
-                    From: document.getElementById("email").value,
-                    Subject: "thandophotography contact form enquiry",
-                    Body: "And this is the body"
-                }).then(
-                    message => alert(message)
-                );
-            }
